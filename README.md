ce9636fb5e3bad3f1764da86590f495325f843ba
Gonzalez, Kevin <kgonzalez15@go.pasadena.edu>
2022-06-02 15:46:25 -0700

    even more startfiles and todos
    I worked on setting a superuser to access the admin panel for django. Finished setting up locations for static files. Created a model.

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # About
-The goal is to practice django by building a social media app.
+Learn Django by building a social media app
 
 ## Tech Stack
 - Django
 - Bootstrap
 - SQL
+
+## Tutorial Author: Code With Tomi
+- https://www.youtube.com/watch?v=xSUm6iMtREA&ab_channel=freeCodeCamp.org
--- socialKey/core/models.py
+++ socialKey/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from numpy import blackman
+
+
+
+# In django anytime you make changes to your models
+# make migrations
+# -python manage.py makemigrations
+
+## AFTER YOU HAVE MADE YOUR MIGRATIONS
+# you will sync [I'm not sure if sync is the proper term]
+# your new migrations with your db
+# -python manage.py migrate
+
+## CREATE AN ADMIN PANEL
+# -python manage.py createsuperuser
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+    # use connects to foreign key
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    # method to return the object's stored name
+    def __str__(self):
+        return self.user.username
